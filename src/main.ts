8e6096d896b9bebc57a1d1f52d16af9dde26168d
softwaredeveloper111 <girisourav702939@gmail.com>
2026-07-18 00:52:51 +0530

    ui(filters):add filters here

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,9 @@ import 'remixicon/fonts/remixicon.css'
 
 
 interface FilterType  {
-  value:number;
-  min:number;
-  max:number;
+  value:string;
+  min:string;
+  max:string;
   unit:string;
 }
 
@@ -16,7 +16,6 @@ interface FilterType  {
 interface Filter<T>{
   brightness:T,
   contrast:T,
-  exposure:T,
   saturation:T,
   hueRotation:T,
   blur:T,
@@ -32,72 +31,99 @@ interface Filter<T>{
 
 const filters:Filter<FilterType> = {
   brightness:{
-    value:100,
-    min:0,
-    max:200,
+    value:"100",
+    min:"0",
+    max:"200",
     unit:"%"
   },
   contrast:{
-    value:100,
-    min:0,
-    max:200,
+    value:"100",
+    min:"0",
+    max:"200",
     unit:"%"
   },
-  exposure:{
-     value:100,
-     min:0,
-     max:200,
-     unit:"%",
 
-  },
   saturation:{
-     value:100,
-     min:0,
-     max:200,
+     value:"100",
+     min:"0",
+     max:"200",
      unit:"%"
   },
   hueRotation:{
-    value:0,
-    min:0,
-    max:360,
+    value:"0",
+    min:"0",
+    max:"360",
     unit:"deg",
   },
   blur:{
-    value:0,
-    min:0,
-    max:20,
+    value:"0",
+    min:"0",
+    max:"20",
     unit:"px"
   },
   grayscale:{
-    value:0,
-    min:0,
-    max:100,
+    value:"0",
+    min:"0",
+    max:"100",
     unit:"%"
   },
   sepia:{
-    value:0,
-    min:0,
-    max:100,
+    value:"0",
+    min:"0",
+    max:"100",
     unit:"%"
   },
   opacity:{
-    value:100,
-    min:0,
-    max:100,
+    value:"100",
+    min:"0",
+    max:"100",
     unit:"%",
   },
   invert:{
-     value:0,
-     min:0,
-     max:100,
+     value:"0",
+     min:"0",
+     max:"100",
      unit:"%"
   }
 }
 
 
 
-function createElement(name,value,min,max,unit="%") {
+function createElement(name:string,value:string,min:string,max:string):HTMLDivElement {
   
-  
+  const div = document.createElement("div") as HTMLInputElement;
+  div.classList.add("filter");
+
+
+  const p = document.createElement("p") as HTMLInputElement;
+  p.textContent = name;
+
+
+  const input = document.createElement("input") as HTMLInputElement;
+  input.type = "range";
+  input.name=  name;
+  input.id= name;
+  input.min = min;
+  input.max = max;
+  input.value = value;
+
+
+  div.appendChild(p);
+  div.appendChild(input);
+  return div
 
 }
+
+
+
+
+Object.keys(filters).forEach(filterName=>{
+  const key = filterName as keyof typeof filters;
+  //  console.log(filterName,filters[key])
+  const filtersContainerDiv = document.getElementById("filters") as HTMLDivElement;
+  filtersContainerDiv.appendChild(createElement(filterName, filters[key].value, filters[key].min, filters[key].max));
+})
+
+
+
+
